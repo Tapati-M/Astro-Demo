b62953269cfd5d1efd4e3dcd88d5abfe7f31fea6
Tapati-M <tapatimondal6162@gmail.com>
2024-11-24 19:16:32 +0530

    Add files via upload

---
+++ src/Pages/ConsultAstrologer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ConsultAstrologer = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white">
+    <h1 className="text-4xl font-bold mb-6">Welcome to the Consult Astrologer</h1>
+    <p className="text-lg">Browse Consult Astrologer!</p>
+  </div>
+  )
+}
+
+export default ConsultAstrologer
